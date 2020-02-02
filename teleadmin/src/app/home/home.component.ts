a2348c175694826d488b07b57128cb00dce94def
Julien JOVENIAUX <julien.joveniaux@gmail.com>
2020-02-02 17:17:44 +0100

    no message

--- teleadmin/src/app/home/home.component.ts
+++ teleadmin/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../environments/environment';
-import { GridGapDirective } from '@angular/flex-layout/grid/typings/gap/gap';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,28 +8,56 @@ import { GridGapDirective } from '@angular/flex-layout/grid/typings/gap/gap';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private fb: FormBuilder) { }
+  loginForm: FormGroup;
   active = true;
   fullAddress: string;
   patient = {
     gender: "m",
-    name: "cule",
+    name: "neymar",
     surname: "jean", 
     address: "35 rue du général leclerc",
     city: "issy",
     zipcode: "92130",
-    mail: "jean.cule@gmail.com",
+    email: "jean.cule@gmail.com",
     phone: "0612345678",
     doctor: "dr Ibert",
     allergy: "nan",
     vaccins: "nan",
-    notes: "ras"
+    notes: "ras",
+    secu: "1234564789"
 
   }
+  doit(){
+    this.loginForm.disable();
+    this.active = true;
+  }
+  doitt(){
+    this.loginForm.enable();
+    this.active = false;
+  }
+
   
+
+ 
+
   ngOnInit() {
+    
     this.fullAddress= `${this.patient.address} , ${this.patient.zipcode} , ${this.patient.city}`;
+    this.loginForm = this.fb.group({
+      gender: [this.patient.gender, [Validators.required, Validators.minLength(3)]],
+      name: [this.patient.name, [Validators.required, Validators.minLength(3)]],
+      surname: [this.patient.surname, [Validators.required, Validators.minLength(3)]],
+      address: [this.patient.address, [Validators.required, Validators.minLength(3)]],
+      email: [this.patient.email, [Validators.required, Validators.minLength(3), Validators.email]],
+      phone: [this.patient.phone, [Validators.required, Validators.minLength(3)]],
+      doctor: [this.patient.doctor, [Validators.required, Validators.minLength(3)]],
+      allergy: [this.patient.allergy, [Validators.required, Validators.minLength(3)]],
+      vaccins: [this.patient.vaccins, [Validators.required, Validators.minLength(3)]],
+      notes: [this.patient.notes, [Validators.required, Validators.minLength(3)]]
+      
+    });
+    this.loginForm.disable();
   }
 
 }
