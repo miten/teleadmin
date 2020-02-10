40051e7e424b2712361532b1b81b4ae72f799ae5
Your Name <you@example.com>
2020-02-10 10:38:46 +0100

    Removed add profile componenent

--- teleadmin/src/app/app.module.ts
+++ teleadmin/src/app/app.module.ts
@@ -22,15 +22,12 @@ import { HomeComponent } from './home/home.component';
 import { SearchBarComponent } from './home/search-bar/search-bar.component';
 import { LightProfileComponent } from './patient/light-profile/light-profile.component';
 import { ProfileComponent } from './patient/profile/profile.component';
-import { AddPatientComponent } from './patient/add-patient/add-patient.component';
 import { LightProfiledComponent } from './employee/light-profiled/light-profiled.component';
 import { LivestreamComponent } from './livestream/livestream.component';
 import { AdminComponent } from './admin/admin.component';
 import { NotificationComponent } from './livestream/notification/notification.component';
 import { MatListModule } from '@angular/material';
 import { CapitalizePipe } from '../pipes/capitalize.pipe';
-
-import { MatNavList, MatListModule } from '@angular/material';
 import {WebcamModule} from 'ngx-webcam';
 
 
@@ -48,7 +45,6 @@ export function tokenGetter() {
     LightProfileComponent,
     LightProfileComponent,
     ProfileComponent,
-    AddPatientComponent,
     LightProfiledComponent,
     LivestreamComponent,
     AdminComponent,
