0d6b952f10516e7858d3b074a6e58a09142f5f6d
Julien JOVENIAUX <julien.joveniaux@gmail.com>
2020-02-10 16:09:27 +0100

    ok

--- teleadmin/src/app/patient/profile/profile.component.ts
+++ teleadmin/src/app/patient/profile/profile.component.ts
@@ -13,8 +13,7 @@ export class ProfileComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private fb: FormBuilder, private patientService: PatientService) {}
 
-
-  fields = ['gender', 'name', 'surname', 'city', 'street', 'email', 'phone'];
+  startDate = new Date(1970, 0, 1);
   patient: Patient;
   loginForm: FormGroup;
 
@@ -36,17 +35,29 @@ export class ProfileComponent implements OnInit {
   loadForm() {
     if (this.patient) {
       this.loginForm = this.fb.group({
-
+        gender: [this.patient.gender, [Validators.maxLength(1)]],
         name: [this.patient.name, [Validators.minLength(3)]],
+        surname: [this.patient.surname, [Validators.minLength(3)]],
+        birthday: [this.patient.birthday, [Validators.required]],
+        city: [this.patient.city, [Validators.minLength(3)]],
+        street: [this.patient.street, [Validators.minLength(3)]],
+        phone: [this.patient.phone, [Validators.maxLength(10)]],
+        email: [this.patient.email, [Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$)")]],
+        secu: [this.patient.secu, [Validators.minLength(15)], [Validators.maxLength(15)]]
 
       });
     } else {
 
-        this.loginForm = this.fb.group({
-
-          name: ['', [Validators.minLength(3)]],
-
-
+      this.loginForm = this.fb.group({
+        gender: ['', [Validators.maxLength(1)]],
+        name: ['', [Validators.minLength(3)]],
+        surname: ['', [Validators.minLength(3)]],
+        birthday: ['', [Validators.required]],
+        city: ['', [Validators.minLength(3)]],
+        street: ['', [Validators.minLength(3)]],
+        phone: ['', [Validators.maxLength(10)]],
+        email: ['', [Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$)")]],
+        secu: ['', [Validators.minLength(15)], [Validators.maxLength(15)]]
         });
     }
   }
